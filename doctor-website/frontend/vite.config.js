811f4729ab4df0775d9257f253abc2e11ed12fef
MahithaRenu6 <edemahitharenu@gmail.com>
2026-02-24 10:45:24 +0530

    Remove proxy settings from Vite config
    Removed proxy configuration from Vite server settings.

--- doctor-website/frontend/vite.config.js
+++ doctor-website/frontend/vite.config.js
@@ -1,16 +1,6 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://https://doctor-website-1-mpw0.onrender.com/api',
-        changeOrigin: true,
-        secure: false,
-      },
-    },
-  },
-})
+});
